8d1df9524684f752af7d2a6f9fd2d2eb9f7d2914
Matthew Jason Craig <themattycraig@gmail.com>
2020-06-29 16:12:44 -0400

    Initial commit

---
+++ components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <b-container fluid="lg" class="d-flex">
+      <span>{{ siteName }}</span>
+      <div class="ml-auto">
+        <nuxt-link class="btn btn-sm btn-link mx-1" to="/">Home</nuxt-link>
+        <nuxt-link class="btn btn-sm btn-link mx-1" to="/about"
+          >About</nuxt-link
+        >
+      </div>
+    </b-container>
+  </header>
+</template>
+
+<script>
+import config from '~/data/siteConfig'
+
+export default {
+  data() {
+    return {
+      siteName: config.siteTitle,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  align-items: center;
+  padding: $spacer 0;
+}
+
+.nuxt-link-exact-active {
+  color: $secondary;
+  font-weight: $font-weight-bold;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layout layout--default">
+    <Header></Header>
+    <main id="content">
+      <b-container fluid="lg">
+        <nuxt></nuxt>
+      </b-container>
+    </main>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+}
+</script>
+
+<style lang="scss">
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+#content {
+  flex: 1 0 auto;
+}
+</style>
